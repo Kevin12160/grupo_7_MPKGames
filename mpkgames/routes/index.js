31c30428cb7c9d344a8465c7b93e8a981ada61d0
Pablodc28 <Pablodc28@gmail.com>
2020-08-12 14:59:33 -0300

    Nav de index y detalle

--- mpkgames/routes/index.js
+++ mpkgames/routes/index.js
@@ -3,24 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+   { title: 'Home' },)
 });
 
 router.get('/detalle', function(req,res){
-  res.render('productDetail')
-})
-
-router.get('/carritoCompras', function(req,res){
-  res.render('productCart')
-})
+  res.render('productDetail', 
+  { title: 'Detalle' });
+});
 
 router.get('/registroUsuarios', function(req,res){
-  res.render('register')
-})
+  res.render('register',
+  { title: 'Registro de usuarios' });
+});
+
+
+router.get('/carritoCompras', function(req,res){
+  res.render('productCart', 
+  { title: 'Carrito de Compras' });
+});
 
 router.get('/AgregarProductos', function(req,res){
-  res.render('productAdd')
-})
+  res.render('productAdd',
+   { title: 'Agregar Productos' });
+});
 
 
 
