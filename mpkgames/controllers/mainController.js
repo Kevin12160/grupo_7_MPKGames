f5e34e751caf02b690498702a5915a33960efb09
Pablodc28 <Pablodc28@gmail.com>
2020-08-24 23:29:01 -0300

    Mejoras en Pagina Home con Datos de Json

---
+++ mpkgames/controllers/mainController.js
@@ -0,0 +1,19 @@
+const dbProduct = require('../data/database') //requiero la base de datos de productos
+
+module.exports = { //exporto un objeto literal con todos los metodos
+    index: function(req, res) {
+        let OfertasUtimosJuegos = dbProduct.filter(producto => {
+            return producto.OfertasUtimosJuegos == "1"
+        })
+        let OfertasDeLaSemana = dbProduct.filter(producto => {
+            return producto.OfertasDeLaSemana == "1"
+        })
+        
+        res.render('index', { //renderizo en el navegador la vista index que contiene el HOME del sitio
+            title: 'Home', //envío el objeto literal con la o las variables necesarias para renderizar de forma correcta el home
+            OfertasUtimosJuegos: OfertasUtimosJuegos,
+            OfertasDeLaSemana: OfertasDeLaSemana
+        })
+    }
+ 
+}
